463e2e2f51ecb9c33a0507d33feb5acba4920294
Alex Lipovka <alex.lipovka@gmail.com>
2023-04-19 17:42:43 +0400

    update

--- sketch_5/sketch.js
+++ sketch_5/sketch.js
@@ -13,6 +13,62 @@ const options = {
 };
 
 
+
+
+let attractors = []; // массив аттракторов
+let movers = []; // массив муверов
+
+// Настройка приложения
+// Данная функция будет выполнена первой и только один раз
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+
+	myMap = mappa.tileMap(options);
+
+	myMap.overlay(canvas);
+
+	// добавляем аттракторы в массив
+	// attractors.push(new Attractor({lat : 45.3515, lng : 36.4695}));
+	// attractors.push(new Attractor({lat: 45.3520, lng:36.4700}));
+	// attractors.push(new Attractor({lat: 45.3525, lng:36.4705}));
+}
+
+// Основная функция отрисовки
+// Выполняется 60 раз в секунду (как правило)
+function draw() {
+	// очищаем экран
+	clear();
+
+	// рисуем аттракторы
+	attractors.forEach(a => a.display());
+
+	// обновляем и рисуем муверов
+	movers.forEach(m => {
+		m.update();
+		m.display();
+	});
+
+	// рисуем курсор мыши
+	fill(255);
+	ellipse(mouseX, mouseY, 21, 21);
+}
+
+// Вспомогательная функция, которая реагирует на изменения размера
+function windowResized() {
+	canvas = resizeCanvas(windowWidth, windowHeight);
+	myMap.mappaDiv.style.width = windowWidth + 'px';
+	myMap.mappaDiv.style.height = windowHeight + 'px';
+}
+
+function mouseClicked() {
+	// добавляем нового мувера при нажатии на Ctrl и клике мыши
+	if (keyIsPressed === true && keyCode === CONTROL) {
+		movers.push(new Mover(myMap.pixelToLatLng(mouseX, mouseY).lat, myMap.pixelToLatLng(mouseX, mouseY).lng));
+	} else if (keyIsPressed === true && keyCode === ALT) {
+		attractors.push(new Attractor(myMap.pixelToLatLng(mouseX, mouseY)));
+	}
+}
+
 // класс мувера
 class Mover {
 	constructor(lat, lng) {
@@ -53,69 +109,8 @@ class Mover {
 
 		// обновляем координаты мувера на карте
 		let latLng = myMap.pixelToLatLng(this.position.x, this.position.y);
-		let pos = myMap.latLngToPixel(latLng);
-		this.position.set(pos.x, pos.y);
-	}
-}
-
-let attractors = []; // массив аттракторов
-let movers = []; // массив муверов
-
-// Настройка приложения
-// Данная функция будет выполнена первой и только один раз
-function setup() {
-	canvas = createCanvas(windowWidth, windowHeight);
-
-	myMap = mappa.tileMap(options);
-
-	myMap.overlay(canvas);
-
-	// добавляем аттракторы в массив
-	attractors.push(new Attractor({lat : 45.3515, lng : 36.4695}));
-	attractors.push(new Attractor({lat: 45.3520, lng:36.4700}));
-	attractors.push(new Attractor({lat: 45.3525, lng:36.4705}));
-
-	// добавляем муверов в массив
-	// movers.push(new Mover(45.3500, 36.4690));
-	// movers.push(new Mover(45.3505, 36.4695));
-	// movers.push(new Mover(45.3510, 36.4700));
-	// movers.push(new Mover(45.3515, 36.4705));
-	// movers.push(new Mover(45.3520, 36.4710));
-	// movers.push(new Mover(45.3525, 36.4715));
-
-}
-
-// Основная функция отрисовки
-// Выполняется 60 раз в секунду (как правило)
-function draw() {
-	// очищаем экран
-	clear();
-
-	// рисуем аттракторы
-	attractors.forEach(a => a.display());
-
-	// обновляем и рисуем муверов
-	movers.forEach(m => {
-		m.update();
-		m.display();
-	});
-
-	// рисуем курсор мыши
-	fill(255);
-	ellipse(mouseX, mouseY, 21, 21);
-}
-
-// Вспомогательная функция, которая реагирует на изменения размера
-function windowResized() {
-	canvas = resizeCanvas(windowWidth, windowHeight);
-	myMap.mappaDiv.style.width = windowWidth + 'px';
-	myMap.mappaDiv.style.height = windowHeight + 'px';
-}
-
-function mouseClicked() {
-	// добавляем нового мувера при нажатии на Ctrl и клике мыши
-	if (keyIsPressed === true && keyCode === CONTROL) {
-		movers.push(new Mover(myMap.pixelToLatLng(mouseX, mouseY).lat, myMap.pixelToLatLng(mouseX, mouseY).lng));
+		// let pos = myMap.latLngToPixel(latLng);
+		// this.position.set(pos.x, pos.y);
 	}
 }
 
@@ -126,27 +121,31 @@ class Attractor {
 		this.geo = geo;
 		// this.geoPosition.lat = geo.lat;
 		// this.geoPosition.lng = geo.lng;
+		// this.position = new p5.Vector(0, 0);
+		// this.pos = myMap.latLngToPixel( this.geo.lat, this.geo.lng ); // преобразуем координаты в пиксели
 		this.position = new p5.Vector(0, 0);
-
-		// this.position = myMap.latLngToPixel( lat, lng ); // преобразуем координаты в пиксели
+		// console.log(this.pos);
 		// console.log(lat, lng);
 		// console.log(myMap);
 		// console.log(this);
-		this.radius = 10;
-		this.color = color(255, 0, 0);
+		this.radius = 100;
+		this.color = color(255, 0, 0, 50);
 	}
+	
 	// метод отрисовки аттрактора
 	display() {
 		noStroke();
 		fill(this.color);
-		this.position = myMap.latLngToPixel(this.geo.lat, this.geo.lng);
-		ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
+		let pos = myMap.latLngToPixel(this.geo.lat, this.geo.lng);
+		ellipse(pos.x, pos.y, this.radius * 2, this.radius * 2);
+		this.position.set(pos.x, pos.y);
 	}
 
 	// метод расчета силы притяжения
 	attract(mover) {
 		// расстояние между аттрактором и мувером
-		let distance = dist(this.position.x, this.position.y, mover.position.x, mover.position.y);
+		let distance = this.position.dist(mover.position);
+		// console.log(distance);
 
 		// если мувер находится внутри радиуса аттрактора, притяжение усиливается
 		if (distance < this.radius) {
@@ -154,7 +153,10 @@ class Attractor {
 		}
 
 		// расчет вектора направления притяжения
-		let force = createVector(this.position.x - mover.position.x, this.position.y - mover.position.y);
+		// let force = new p5.Vector(this.position.x - mover.position.x, this.position.y - mover.position.y);
+		console.log(this.position);
+		let force = this.position.copy();
+		force.sub(mover.position);
 
 		// расчет величины притяжения
 		let strength = (5 * mover.mass * this.radius) / (distance * distance);
